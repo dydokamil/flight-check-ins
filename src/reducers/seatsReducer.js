32e11aab3a8d2c04e37c8ebce5243d93d00dc5e8
dydokamil <dydokamil@gmail.com>
2018-05-06 11:47:19 +0200

    Created tests for rootreducer

--- src/reducers/seatsReducer.js
+++ src/reducers/seatsReducer.js
@@ -1,6 +1,3 @@
-// import _ from'lodash'
-import moment from "moment"
-
 import * as types from "../constants/ActionTypes"
 
 const INITIAL_STATE = {
---
+++ src/services/__tests__/rootReducer.test.js
@@ -0,0 +1,22 @@
+import { createStore, applyMiddleware } from "redux"
+
+import rootReducer from "../../reducers"
+import seatsReducer from "../../reducers/seatsReducer"
+import sessionReducer from "../../reducers/sessionReducer"
+import reservationReducer from "../../reducers/reservationReducer"
+
+describe("root reducer", () => {
+  it("should test the root reducer", () => {
+    const store = createStore(rootReducer)
+
+    expect(store.getState().seatsReducer).toEqual(seatsReducer(undefined, {}))
+    expect(store.getState().sessionReducer).toEqual(
+      sessionReducer(undefined, {}),
+    )
+    expect(store.getState().reservationReducer).toEqual(
+      reservationReducer(undefined, {}),
+    )
+
+    expect(Object.keys(store.getState()).length).toEqual(3)
+  })
+})
